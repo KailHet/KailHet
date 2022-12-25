0bdddb1eb60ebe733278e751d9ba6c8c18b6aa84
KailHet <71993307+KailHet@users.noreply.github.com>
2022-12-26 03:13:04 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+const preloader = document.querySelector(".preloader");
+const button = document.querySelector(".preloader-content_button");
+let animInterval;
+let loadInterval;
+
+const videoId = "2hX8CYck998"
+YT.ready(_=>{
+    window.player = new YT.Player("player", {
+        videoId: videoId,
+        playerVars: { "autoplay": 0, "controls": 0, "loop": 1, "playlist": videoId},
+        events: {
+            "onReady": e => {
+                e.target.setVolume(100);
+                clearInterval(loadInterval)
+                button.classList.add("ready");
+                button.addEventListener("click", () => {
+                    animInterval = setInterval(preloaderAnim, 10);
+                    button.classList.add("clicked");
+                });
+            }
+        }
+    });
+})
+
+let i = 0;
+
+let j = 1;
+function preloaderAnim() {
+    j -= .01;
+    preloader.style.opacity = j;
+
+    if (j <= 0) {
+        clearInterval(animInterval);
+        setTimeout(() => {preloader.removeAttribute("style");}, 50);
+        setTimeout(() => {preloader.style.display = "none"; preloader.remove()}, 60);
+    };
+
+    if (j <= 1) {
+        player.unMute();
+        player.playVideo();
+    };
+};
