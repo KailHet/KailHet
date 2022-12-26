d8d85ceb45d66488050bbf61780dcaaed11c04de
KailHet <71993307+KailHet@users.noreply.github.com>
2022-12-26 21:47:31 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const button = document.querySelector(".preloader-content_button");
 let animInterval;
 let loadInterval;
 
-const videoId = "2hX8CYck998"
+const videoId = "0WhMflvnmrU"
 YT.ready(_=>{
     window.player = new YT.Player("player", {
         videoId: videoId,
